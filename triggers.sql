55ad90c1f089f0ec88a6e0bb52451c7227eb6cc5
Urja Sahni <125489370+UrjaSahni@users.noreply.github.com>
2025-10-27 19:00:09 +0530

    Add triggers for book issuance and return

--- triggers.sql
+++ triggers.sql
@@ -1,9 +1,10 @@
+1.
 CREATE OR REPLACE TRIGGER book_issuance_trigger
 AFTER INSERT ON issue
 FOR EACH ROW
     VALUES (:NEW.pkb, :NEW.pkr, :NEW.pkb, :NEW.issue_date, (SELECT Staff_id FROM track WHERE User_id = :NEW.pkr));
 
-
+2.
 CREATE OR REPLACE TRIGGER book_return_trigger
 BEFORE UPDATE OF return_date ON issue
 FOR EACH ROW
